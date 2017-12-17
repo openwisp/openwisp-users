07008913ad4b1823da27197111f465a5b37aa764
gandhi23 <34238569+gandhi23@users.noreply.github.com>
2017-12-17 17:12:00 +0530

    Update urls.py

--- tests/urls.py
+++ tests/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include, path
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include('adminsite.urls')),
     url(r'^accounts/', include('allauth.urls')),
 ]
 
