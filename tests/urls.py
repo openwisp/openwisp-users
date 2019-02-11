43ef673858d75055f1ae78aa078ad75a14a01d4d
vedangj044 <vedangj044@gmail.com>
2019-02-12 00:23:03 +0530

    [tests/urls.py/] added redirect_view to urls, to directly land on '/admin' page.# Please enter the commit message for your changes. Lines starting A  small enhancement so that user is redirected to admin page directly on launching the server.

--- tests/urls.py
+++ tests/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import reverse_lazy
+from django.views.generic import RedirectView
+
+redirect_view = RedirectView.as_view(url=reverse_lazy('admin:index'))
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('openwisp_users.accounts.urls')),
+    url(r'^$', redirect_view, name='index')
 ]
 
 urlpatterns += staticfiles_urlpatterns()
