3040de04bd5e2a16333bf6220100540ab851fc67
vedangj044 <vedangj044@gmail.com>
2019-02-12 00:09:13 +0530

    Bad commit Revert "added redirect_view to urls, to directly land on '/admin' page."
    This reverts commit a68d69395380578f3da2b70cfdd97879fe646390.

--- tests/urls.py
+++ tests/urls.py
@@ -1,15 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.views.generic import RedirectView
-from django.urls import reverse_lazy
-
-redirect_view = RedirectView.as_view(url=reverse_lazy('admin:index'))
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('openwisp_users.accounts.urls')),
-    url(r'^$', redirect_view, name='index')
 ]
 
 urlpatterns += staticfiles_urlpatterns()
