9f9f3aad1e13b5551d525bb520011d545092101a
Federico Capoano <nemesis@ninux.org>
2019-04-25 17:53:29 -0400

    [admin] Added menu items

--- openwisp_users/apps.py
+++ openwisp_users/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -6,3 +7,19 @@ class OpenwispUsersConfig(AppConfig):
     name = 'openwisp_users'
     app_label = 'openwisp_users'
     verbose_name = _('Users and Organizations')
+
+    def ready(self, *args, **kwargs):
+        super(OpenwispUsersConfig, self).ready(*args, **kwargs)
+        self.add_default_menu_items()
+
+    def add_default_menu_items(self):
+        menu_setting = 'OPENWISP_DEFAULT_ADMIN_MENU_ITEMS'
+        items = [
+            {'model': 'openwisp_users.User'},
+            {'model': 'openwisp_users.Organization'},
+        ]
+        if not hasattr(settings, menu_setting):
+            setattr(settings, menu_setting, items)
+        else:
+            current_menu = getattr(settings, menu_setting)
+            current_menu += items
