86223bb3d9c421991e0c4969136f4e2efee65071
Federico Capoano <federico.capoano@gmail.com>
2020-05-15 21:00:23 -0500

    [api] Made obtain token view browsable #116
    Related to #116

--- openwisp_users/api/views.py
+++ openwisp_users/api/views.py
@@ -1,10 +1,15 @@
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from .throttling import AuthRateThrottle
 
 
 class ObtainAuthTokenView(ObtainAuthToken):
     throttle_classes = [AuthRateThrottle]
+    authentication_classes = []
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    metadata_class = api_settings.DEFAULT_METADATA_CLASS
+    versioning_class = api_settings.DEFAULT_VERSIONING_CLASS
 
 
 obtain_auth_token = ObtainAuthTokenView.as_view()
