9d55c312899af146c771b9f4ffbb2cf31ad5b7ed
Federico Capoano <federico.capoano@gmail.com>
2020-07-25 19:00:28 -0500

    [tests] Show queries in console while testing manually

--- tests/openwisp2/settings.py
+++ tests/openwisp2/settings.py
@@ -87,6 +87,34 @@ TEMPLATES = [
     }
 ]
 
+if not TESTING:
+    LOGGING = {
+        'disable_existing_loggers': False,
+        'version': 1,
+        'handlers': {
+            'console': {
+                # logging handler that outputs log messages to terminal
+                'class': 'logging.StreamHandler',
+                'level': 'DEBUG',  # message level to be written to console
+            },
+        },
+        'loggers': {
+            '': {
+                # this sets root level logger to log debug and higher level
+                # logs to console. All other loggers inherit settings from
+                # root level logger.
+                'handlers': ['console'],
+                'level': 'DEBUG',
+                'propagate': False,
+            },
+            'django.db': {
+                'level': 'DEBUG',
+                'handlers': ['console'],
+                'propagate': False,
+            },
+        },
+    }
+
 # during development only
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 LOGIN_REDIRECT_URL = 'admin:index'
