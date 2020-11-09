f1fc791864b61c3874dc9523bbcd42a7987cd969
niteshsinha17 <niteshsinha1707@gmail.com>
2020-11-09 13:46:05 +0530

    [fix] Allow swagger to show parameters of obtain token view #116
    closes #116

--- openwisp_users/api/views.py
+++ openwisp_users/api/views.py
@@ -1,6 +1,8 @@
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
+from .swagger import ObtainTokenRequest, ObtainTokenResponse
 from .throttling import AuthRateThrottle
 
 
@@ -10,6 +12,12 @@ class ObtainAuthTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
     metadata_class = api_settings.DEFAULT_METADATA_CLASS
     versioning_class = api_settings.DEFAULT_VERSIONING_CLASS
+
+    @swagger_auto_schema(
+        request_body=ObtainTokenRequest, responses={200: ObtainTokenResponse}
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
 
 
 obtain_auth_token = ObtainAuthTokenView.as_view()
