dcc3dfe3b6a5f900e10ad236ef9f59138293a397
sankalp <sankalp123427@gmail.com>
2020-11-14 00:30:10 +0530

    [change] Removed typographic error for backward compatibility #194
    Removed all the typographic errors for 0.5 release.
    
    i.e. changed `ORGANIZATON` into `ORGANIZATION`.`
    
    Fixes #194

--- openwisp_users/settings.py
+++ openwisp_users/settings.py
@@ -1,18 +1,8 @@
 from django.conf import settings
 from openwisp_utils.utils import default_or_test
 
-ORGANIZATION_USER_ADMIN = (
-    # setting with typographic error for maintaining backward compatibility
-    # TODO: remove in openwisp-users 0.5
-    getattr(settings, 'OPENWISP_ORGANIZATON_USER_ADMIN', False)
-    or getattr(settings, 'OPENWISP_ORGANIZATION_USER_ADMIN', False)
-)
-ORGANIZATION_OWNER_ADMIN = (
-    # setting with typographic error for maintaining backward compatibility
-    # TODO: remove in openwisp-users 0.5
-    getattr(settings, 'OPENWISP_ORGANIZATON_OWNER_ADMIN', True)
-    or getattr(settings, 'OPENWISP_ORGANIZATON_OWNER_ADMIN', True)
-)
+ORGANIZATION_USER_ADMIN = getattr(settings, 'OPENWISP_ORGANIZATION_USER_ADMIN', False)
+ORGANIZATION_OWNER_ADMIN = getattr(settings, 'OPENWISP_ORGANIZATION_OWNER_ADMIN', True)
 USERS_AUTH_API = getattr(settings, 'OPENWISP_USERS_AUTH_API', False)
 USERS_AUTH_THROTTLE_RATE = getattr(
     settings,
