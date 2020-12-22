ebcfaf38e696e5ef9c367ee124cefd1389361bc0
Noumbissi Valere <noumbissivalere@gmail.com>
2020-12-22 09:24:29 +0100

    [fix] Fixed possible security issues

--- openwisp_users/backends.py
+++ openwisp_users/backends.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
+from django.core.exceptions import ValidationError
 from django.db.models import Q
 
 User = get_user_model()
@@ -10,7 +11,25 @@ class UserAuthenticationBackend(ModelBackend):
         users = User.objects.filter(
             Q(email=username) | Q(phone_number=username) | Q(username=username)
         )
-        for user in users:
-            if user.check_password(password) and self.user_can_authenticate(user):
-                return user
+        # can not use users.first() because it orders the queryset before
+        # returning the first object which is not what we want
+        user = users[0] if users else None
+        if user and user.check_password(password) and self.user_can_authenticate(user):
+            return user
         return None
+
+    def get_user(self, identifier):
+        """
+        returns an active user with the identifier supplied
+        """
+        try:
+            # By default, primary key is provided as identifier
+            # during login and logout
+            return User.objects.get(pk=identifier, is_active=True)
+        except (User.DoesNotExist, ValidationError):
+            users = User.objects.filter(
+                Q(email=identifier)
+                | Q(phone_number=identifier)
+                | Q(username=identifier)
+            ).filter(is_active=True)
+            return users[0] if users else None
--- openwisp_users/tests/test_backends.py
+++ openwisp_users/tests/test_backends.py
@@ -3,7 +3,11 @@ from uuid import UUID
 from django.test import TestCase
 from django.test.utils import override_settings
 
+from openwisp_users.backends import UserAuthenticationBackend
+
 from .utils import TestOrganizationMixin
+
+auth_backend = UserAuthenticationBackend()
 
 
 class TestBackends(TestOrganizationMixin, TestCase):
@@ -39,37 +43,66 @@ class TestBackends(TestOrganizationMixin, TestCase):
         AUTHENTICATION_BACKENDS=('openwisp_users.backends.UserAuthenticationBackend',)
     )
     def test_user_with_email_as_username_auth_backend(self):
-        user1 = self._create_user(
+        user = self._create_user(
             username='tester',
             email='tester@gmail.com',
             phone_number='+237675579231',
             password='tester',
         )
-        user = self._create_user(
+        self._create_user(
             username='tester@gmail.com',
             email='tester1@gmail.com',
             phone_number='+237675579232',
             password='tester1',
         )
-        self._test_user_auth_backend_helper(user.username, 'tester1', user.pk)
-        self._test_user_auth_backend_helper(user1.email, 'tester', user1.pk)
+        self._test_user_auth_backend_helper(user.email, 'tester', user.pk)
 
     @override_settings(
         AUTHENTICATION_BACKENDS=('openwisp_users.backends.UserAuthenticationBackend',)
     )
     def test_user_with_phone_number_as_username_auth_backend(self):
-        user1 = self._create_user(
+        user = self._create_user(
             username='tester',
             email='tester@gmail.com',
             phone_number='+237675579231',
             password='tester',
         )
-        user = self._create_user(
+        self._create_user(
             username='+237675579231',
             email='tester1@gmail.com',
             phone_number='+237675579232',
             password='tester1',
         )
+        self._test_user_auth_backend_helper(user.phone_number, 'tester', user.pk)
 
-        self._test_user_auth_backend_helper(user.username, 'tester1', user.pk)
-        self._test_user_auth_backend_helper(user1.phone_number, 'tester', user1.pk)
+    def test_auth_backend_get_user(self):
+        user = self._create_user(
+            username='tester',
+            email='tester@gmail.com',
+            phone_number='+237675579231',
+            password='tester',
+        )
+        user1 = self._create_user(
+            username='tester1',
+            email='tester1@gmail.com',
+            phone_number='+237675579232',
+            password='tester1',
+        )
+        with self.subTest('get user with primary key'):
+            self.assertEqual(auth_backend.get_user(user.pk), user)
+
+        with self.subTest('get user with invalid identifier'):
+            self.assertIsNone(auth_backend.get_user('invalid'))
+
+        with self.subTest('get user with email'):
+            user1.username = user.email
+            user1.save()
+            self.assertEqual(auth_backend.get_user(user.email), user)
+
+        with self.subTest('get user with phone_number'):
+            user1.username = user.phone_number
+            user1.save()
+            self.assertEqual(auth_backend.get_user(user.phone_number), user)
+
+        with self.subTest('get user with username'):
+            self.assertEqual(auth_backend.get_user(user.username), user)
