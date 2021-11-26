4b50c625d999f684a5ef6b7a622815cddb89a390
Federico Capoano <f.capoano@openwisp.io>
2021-11-26 12:37:46 -0300

    [fix] Updated migration number

--- openwisp_users/migrations/0017_user_language.py
+++ openwisp_users/migrations/0017_user_language.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('openwisp_users', '0014_user_notes'),
+        ('openwisp_users', '0016_organizationinvitation'),
     ]
 
     operations = [
