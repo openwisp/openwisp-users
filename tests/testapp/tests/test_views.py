325c4bebfa1bde8a5ceb648de1f42c8fe109e463
Gagan Deep <pandafy.dev@gmail.com>
2022-12-29 18:38:44 +0530

    [tests] Added helper function

--- tests/testapp/tests/test_views.py
+++ tests/testapp/tests/test_views.py
@@ -16,31 +16,16 @@ class TestAutocompleteJsonView(
     TestMultitenantAdminMixin, TestOrganizationMixin, TestCase
 ):
     def test_autocomplete_view_organization_filter(self):
-        url = reverse('admin:ow-auto-filter')
-        payload = {
-            'app_label': 'testapp',
-            'model_name': 'book',
-            'field_name': 'organization',
-        }
         org1 = self._create_org(name='org1')
         org2 = self._create_org(name='org2')
-
-        with self.subTest('Test with superuser'):
-            admin = self._get_admin()
-            self.client.force_login(admin)
-            response = self.client.get(url, payload)
-            for option in response.json()['results']:
-                assert option['id'] in [
-                    str(id) for id in Organization.objects.values_list('id', flat=True)
-                ]
-
-        with self.subTest('Test with organization user'):
-            administrator = self._create_administrator(organizations=[org1])
-            self.client.force_login(administrator)
-            response = self.client.get(url, payload)
-            for option in response.json()['results']:
-                assert option['id'] in [str(org1.id)]
-                assert option['id'] not in [str(org2.id)]
+        self._get_admin()
+        self._create_administrator(organizations=[org1])
+        self._test_multitenant_admin(
+            url=self._get_autocomplete_view_path('testapp', 'book', 'organization'),
+            visible=[org1.name],
+            hidden=[org2.name],
+            administrator=True,
+        )
 
     @capture_stderr()
     def test_autocomplete_view_blank_option(self):
