509bd2ffe86b5ee7b51feaf338985572117d4371
kaushikaryan04 <kaushik.aryan04@gmail.com>
2024-05-23 20:56:15 +0530

    [openwisp_users/apps.py] fixes issue #357
    When the status ( is_active ) of an organization changes it now invalidates the cache of all the organizationsusers.

--- openwisp_users/apps.py
+++ openwisp_users/apps.py
@@ -3,7 +3,7 @@ import logging
 from django.apps import AppConfig
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
+from django.core.exceptions import ValidationError,ObjectDoesNotExist
 from django.db import IntegrityError, transaction
 from django.db.models.signals import post_delete, post_save, pre_save
 from django.utils.translation import gettext_lazy as _
@@ -98,10 +98,17 @@ class OpenwispUsersConfig(AppConfig):
     def connect_receivers(self):
         OrganizationUser = load_model('openwisp_users', 'OrganizationUser')
         OrganizationOwner = load_model('openwisp_users', 'OrganizationOwner')
+        Organization = load_model("openwisp_users" , "Organization") 
         signal_tuples = [
             (post_save, 'post_save'),
             (post_delete, 'post_delete'),
         ]
+
+        pre_save.connect(
+            self.handle_organization_update,
+            sender = Organization,
+            dispatch_uid='handle_organization_is_active_change'
+        )
 
         for model in [OrganizationUser, OrganizationOwner]:
             for signal, name in signal_tuples:
@@ -129,6 +136,22 @@ class OpenwispUsersConfig(AppConfig):
             sender=OrganizationUser,
             dispatch_uid='make_first_org_user_org_owner',
         )
+
+    @classmethod
+    def handle_organization_update(cls , instance , sender,**kwargs)  :
+        Organization = load_model("openwisp_users" , "Organization")
+        OrganizationUsers = load_model("openwisp_users" ,"OrganizationUser")
+        try:
+            old_obj = Organization.objects.get(pk=instance.pk)
+        except ObjectDoesNotExist:
+            logger.warning(f"Organization with pk {instance.pk} does not exist.")
+            return
+        except Exception as e:
+            logger.exception(f"An error occurred while fetching the organization: {e}")
+            return
+        if getattr(old_obj , "is_active") != getattr(instance , "is_active") :
+            for user in OrganizationUsers.objects.filter(organization = instance):
+                cls._invalidate_user_cache(user)
 
     @classmethod
     def pre_save_update_organizations_dict(cls, instance, **kwargs):
